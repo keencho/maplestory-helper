fbdd4218b1d3cc2b4481692cf629923cc54819bc
keencho <seyoung050412@gmail.com>
2022-12-16 15:27:46 +0900

    update - 링크스킬 준비 - 메뉴 일원화

--- src/model/path.model.ts
+++ src/model/path.model.ts
@@ -2,4 +2,5 @@ export default class Path {
 	static INDEX = '/'
 	static HOMEWORK = '/homework'
 	static BOSS_SOUL_CRYSTAL_CALCULATOR = '/bossSoulCrystalCalculator'
+	static LINK_SKILL = '/linkSkill'
 }
---
+++ src/ui/container/LinkSkillContainer.tsx
@@ -0,0 +1,15 @@
+import PageTitle from '../component/common/PageTitle';
+import React from 'react';
+
+const LinkSkillContainer = () => {
+	return (
+		<>
+			<PageTitle
+				title={'링크스킬'}
+				marginBottom={'.5rem'}
+			/>
+		</>
+	)
+}
+
+export default LinkSkillContainer
