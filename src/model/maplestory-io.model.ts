6f86a6f3ae37eef1c183a5ccc5468274572c8558
keencho <seyoung050412@gmail.com>
2023-07-30 18:03:49 +0900

    bump up version

--- src/model/maplestory-io.model.ts
+++ src/model/maplestory-io.model.ts
@@ -1,4 +1,4 @@
 export const baseURL = 'https://maplestory.io';
 export const region = 'KMST';
-export const version = '1155';
+export const version = '1157';
 export const cacheName = `${region}-${version}`;
